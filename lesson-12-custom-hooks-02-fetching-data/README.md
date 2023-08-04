13b8b9b61027c1f4a046f83a20fea86588f61126
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2023-08-04 22:17:03 +0300

    lesson-12-custom hooks

---
+++ lesson-12-custom-hooks-02-fetching-data/README.md
@@ -0,0 +1,2 @@
+sudo npm install @tanstack/react-query
+sudo npm install axios;
---
+++ lesson-12-custom-hooks-02-fetching-data/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Cat } from './components/Cat';
+function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
+  return (
+    <div className="App">
+           <QueryClientProvider client={client}>
+              <Cat />
+        </QueryClientProvider>
+    </div>
+  );
+}
+
+
+export default App;
